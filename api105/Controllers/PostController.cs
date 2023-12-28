6f01c098ee84752ef21d607c34a8c577d25a3dab
Rumman-416 <chowdhuryrumman78@gmail.com>
2023-12-29 00:26:37 +0530

    initial commit

---
+++ api105/Controllers/PostController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using api105.Models;
+
+[Route("api/projects")]
+public class PostController : ControllerBase
+{
+    private const string JsonFilePath = "Projects.json";
+
+    [HttpGet]
+    public ActionResult<IEnumerable<Post>> GetPosts()
+    {
+        var posts = ReadPostsFromJsonFile();
+        return Ok(posts);
+    }
+
+    private List<Post> ReadPostsFromJsonFile()
+    {
+        if (System.IO.File.Exists(JsonFilePath))
+        {
+            var json = System.IO.File.ReadAllText(JsonFilePath);
+            return JsonConvert.DeserializeObject<List<Post>>(json);
+        }
+        return new List<Post>();
+    }
+}
---
+++ api105/Startup.cs
@@ -0,0 +1,59 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api105
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors(); // Add this line to enable CORS
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseCors(builder => // Enable CORS with more flexible settings for development
+            {
+                builder.AllowAnyOrigin()
+                       .AllowAnyMethod()
+                       .AllowAnyHeader();
+            });
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
